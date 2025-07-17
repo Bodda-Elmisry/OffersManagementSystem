27683cb1a4645944d47c2b09fc22ef8fc4c2bdab
Bodda-Elmisry <elmasry_abdallh@yahoo.com>
2025-07-17 15:38:48 +0300

    Initial commit

---
+++ OffersManagementSystem.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OffersManagementSystem.Domain.Entities
+{
+    public class ApplicationUser
+    {
+    }
+}
