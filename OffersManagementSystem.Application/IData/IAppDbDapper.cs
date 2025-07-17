963c8b0fdcbeb2aaacfd91349bc4a979582151cc
Bodda-Elmisry <elmasry_abdallh@yahoo.com>
2025-07-17 19:30:01 +0300

    add methods to dapper interface

--- OffersManagementSystem.Application/IData/IAppDbDapper.cs
+++ OffersManagementSystem.Application/IData/IAppDbDapper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,14 @@ namespace OffersManagementSystem.Application.IData
 {
     public interface IAppDbDapper<T> where T : class
     {
+        Task<int> ExecuteAsync(string sql, object param = null);
+
+        Task<DbDataReader> ExecuteReaderAsync(string sql, object param = null);
+
+        Task<T> ExecuteScalarAsync(string sql, object param = null);
+
+        Task<IEnumerable<T>> QueryAsync(string sql, object param = null);
+
+        Task<T> QueryFirstOrDefaultAsync(string sql, object param = null);
     }
 }
