581303524f2915e0b6dc17b9b35e93b3064ca6a5
Bodda-Elmisry <elmasry_abdallh@yahoo.com>
2025-07-21 01:57:16 +0300

    update requerd mesage for register

--- OffersManagementSystem.Web/DTOs/RegisterDTO.cs
+++ OffersManagementSystem.Web/DTOs/RegisterDTO.cs
@@ -6,9 +6,9 @@ namespace OffersManagementSystem.Web.DTOs
     {
         [Required(ErrorMessage = "Username is required")]
         public string UserName { get; set; }
-        [Required(ErrorMessage = "Username is required")]
+        [Required(ErrorMessage = "Passord is required")]
         public string Password { get; set; }
-        [Required(ErrorMessage = "Username is required")]
+        [Required(ErrorMessage = "Confirm password is required")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
         public bool IsAdmin { get; set; }
